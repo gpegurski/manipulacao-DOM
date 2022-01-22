a0415d6e9d73172831d9a3004919521c33ef8661
gpegurski <grazipegurski@gmail.com>
2022-01-22 02:36:50 -0300

    Fazendo funcionar o Dark Mode

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function changeMode() {
+	changeClasses();
+}
+
+function changeClasses() {
+	button.classList.toggle(darkModeClass);
+	h1.classList.toggle(darkModeClass);
+	body.classList.toggle(darkModeClass);
+	footer.classList.toggle(darkModeClass);
+}
+
+const darkModeClass = 'dark-mode';
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+button.addEventListener('click', changeMode);
